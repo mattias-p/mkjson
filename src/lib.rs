153e0d1e6c752115ee8f67eb7e3895dcb9b76778
Mattias Päivärinta <mattias.paivarinta@iis.se>
2025-06-25 23:59:28 +0200

    Validate key escaping consistency

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,63 @@
 pub mod parser;
 
+use crate::parser::Assignment;
+use crate::parser::Path;
+use crate::parser::Segment;
 use crate::parser::parse;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::rc::Rc;
 
 pub fn transform<'a>(assignments: impl Iterator<Item = String>) -> Result<String, String> {
     let mut results = vec![];
     for assignment in assignments {
-        let assignment = parse(&assignment).map_err(|e| {
-            format!(
-                "assignment \"{}\": {}",
-                assignment.escape_default(),
-                e.inner
-            )
-        })?;
-        results.push(format!("{:?}", assignment));
+        let assignment = parse(&assignment)
+            .map_err(|e| format!("assignment \"{}\": {}", assignment.escape_default(), e))?;
+        results.push(assignment);
     }
+
+    check(results.as_slice()).map_err(|e| format!("{}", e))?;
+
+    let results: Vec<_> = results
+        .into_iter()
+        .map(|assignment| format!("{:?}", assignment))
+        .collect();
     Ok(results.join("\n"))
+}
+
+pub fn check(assignments: &[Assignment]) -> Result<(), String> {
+    check_key_consistency(assignments)
+}
+
+fn check_key_consistency(assignments: &[Assignment]) -> Result<(), String> {
+    let mut keys: HashMap<Rc<Path>, Rc<Segment>> = HashMap::new();
+    for assignment in assignments {
+        let mut given_path = assignment.path.clone();
+        let mut normalized_path = given_path.normalize();
+
+        while let Some((given_prefix, given_segment)) = given_path.split_last() {
+            if let Segment::Key(_) = &*given_segment {
+                match keys.entry(normalized_path.clone()) {
+                    Entry::Vacant(vacant) => {
+                        vacant.insert(given_segment.clone());
+                    }
+                    Entry::Occupied(occupied) => {
+                        if given_segment != *occupied.get() {
+                            Err(format!(
+                                "path {} has equivalent but inconsistently escaped keys {} and {}",
+                                given_prefix,
+                                occupied.get(),
+                                given_segment
+                            ))?;
+                        }
+                    }
+                }
+            }
+            given_path = given_prefix;
+            normalized_path = normalized_path
+                .prefix()
+                .expect("normalized_path should track given_path");
+        }
+    }
+    Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 use serde_json::Deserializer;
 use serde_json::Value;
 use std::num::ParseIntError;
+use std::rc::Rc;
 use unicode_ident::is_xid_continue;
 use unicode_ident::is_xid_start;
 
@@ -102,10 +103,82 @@ fn escape_string(s: &str) -> String {
         .collect()
 }
 
+fn unescape_string(s: &str) -> String {
+    #[derive(Clone, Copy)]
+    enum State {
+        Normal,
+        Escaped,
+        Hexcode0,
+        Hexcode1,
+        Hexcode2,
+        Hexcode3,
+    }
+    impl State {
+        fn next(self) -> Self {
+            match self {
+                State::Normal => State::Escaped,
+                State::Escaped => State::Hexcode0,
+                State::Hexcode0 => State::Hexcode1,
+                State::Hexcode1 => State::Hexcode2,
+                State::Hexcode2 => State::Hexcode3,
+                State::Hexcode3 => State::Normal,
+            }
+        }
+    }
+    let mut state = State::Normal;
+    let mut acc = 0u32;
+    let unescaped = s.chars().flat_map(|c| match (state, c) {
+        (State::Normal, '\\') => {
+            state = State::Escaped;
+            vec![]
+        }
+        (State::Normal, _)
+        | (State::Escaped, '"')
+        | (State::Escaped, '\\')
+        | (State::Escaped, '/') => {
+            state = State::Normal;
+            vec![c]
+        }
+        (State::Escaped, 'b') => vec![char::from_u32(0x08).expect("valid codepoint")],
+        (State::Escaped, 'f') => vec![char::from_u32(0x0c).expect("valid codepoint")],
+        (State::Escaped, 'n') => vec![char::from_u32(0x0a).expect("valid codepoint")],
+        (State::Escaped, 'r') => vec![char::from_u32(0x0d).expect("valid codepoint")],
+        (State::Escaped, 't') => vec![char::from_u32(0x09).expect("valid codepoint")],
+        (State::Escaped, 'u') => {
+            state = State::Hexcode0;
+            vec![]
+        }
+        (State::Hexcode0 | State::Hexcode1 | State::Hexcode2, _) => {
+            state = state.next();
+            acc = acc << 4
+                | c.to_digit(16)
+                    .expect("caller is responsible for only unescaping valid strings");
+            vec![]
+        }
+        (State::Hexcode3, _) => {
+            state = State::Normal;
+            let unescaped = char::from_u32(
+                acc << 4
+                    | c.to_digit(16)
+                        .expect("caller is responsible for only unescaping valid strings"),
+            )
+            .expect("caller is responsible for only unescaping valid strings");
+            acc = 0;
+            vec![unescaped]
+        }
+        _ => unreachable!(),
+    });
+    Some('"')
+        .into_iter()
+        .chain(unescaped)
+        .chain(Some('"').into_iter())
+        .collect()
+}
+
 #[derive(Debug)]
 pub struct Assignment {
-    path: Vec<Segment>,
-    value: String,
+    pub path: Rc<Path>,
+    pub value: String,
 }
 
 impl From<AssignmentAst> for Assignment {
@@ -120,10 +193,28 @@ impl From<AssignmentAst> for Assignment {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, Hash, PartialEq)]
 pub enum Segment {
     Index(u32),
     Key(String),
+}
+
+impl Segment {
+    pub fn normalize(self: &Rc<Self>) -> Rc<Segment> {
+        match &**self {
+            Segment::Key(key) => Rc::new(Segment::Key(unescape_string(key))),
+            _ => self.clone(),
+        }
+    }
+}
+
+impl std::fmt::Display for Segment {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Segment::Index(index) => write!(f, "{}", index),
+            Segment::Key(key) => write!(f, "{}", key),
+        }
+    }
 }
 
 impl From<SegmentAst> for Segment {
@@ -133,6 +224,73 @@ impl From<SegmentAst> for Segment {
             SegmentAst::Key(key) => Segment::Key(key),
             SegmentAst::Identifier(identifier) => Segment::Key(escape_string(&identifier)),
         }
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Path {
+    Root,
+    Append(Rc<Path>, Rc<Segment>),
+}
+
+impl Path {
+    pub fn root() -> Rc<Self> {
+        Rc::new(Path::Root)
+    }
+
+    pub fn append<T: Into<Rc<Segment>>>(self: &Rc<Self>, segment: T) -> Rc<Self> {
+        Rc::new(Path::Append(self.clone(), segment.into()))
+    }
+
+    pub fn prefix(&self) -> Option<Rc<Path>> {
+        match self {
+            Path::Root => None,
+            Path::Append(prefix, _) => Some(prefix.clone()),
+        }
+    }
+
+    pub fn split_last(&self) -> Option<(Rc<Path>, Rc<Segment>)> {
+        match self {
+            Path::Root => None,
+            Path::Append(prefix, segment) => Some((prefix.clone(), segment.clone())),
+        }
+    }
+
+    pub fn normalize(self: &Rc<Self>) -> Rc<Self> {
+        match &**self {
+            Path::Root => self.clone(),
+            Path::Append(prefix, key) => prefix.normalize().append(key.normalize()),
+        }
+    }
+}
+
+impl std::fmt::Display for Path {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        if let Some((prefix, segment)) = self.split_last() {
+            fn aux(f: &mut std::fmt::Formatter, path: &Rc<Path>) -> std::fmt::Result {
+                match &**path {
+                    Path::Root => Ok(()),
+                    Path::Append(prefix, segment) => {
+                        aux(f, prefix)?;
+                        write!(f, "{}.", segment)
+                    }
+                }
+            }
+            aux(f, &prefix)?;
+            write!(f, "{}.", segment)
+        } else {
+            write!(f, ".")
+        }
+    }
+}
+
+impl FromIterator<Segment> for Rc<Path> {
+    fn from_iter<T>(iter: T) -> Self
+    where
+        T: IntoIterator<Item = Segment>,
+    {
+        iter.into_iter()
+            .fold(Path::root(), |path, segment| path.append(segment))
     }
 }
 
@@ -277,3 +435,8 @@ fn parse_operator(input: &str) -> ParseResult<OperatorAst> {
         Err(ParseError::new(0, "unexpected end of string".to_string()))
     }
 }
+
+// TESTS:
+//  * Accept leading BOM
+//  * Test behavior around unpaired UTF-16 surrogate (e.g., \uDEAD)
+//  * Reject objects with conflicting representations of the same key (e.g., "a\\b" and "a\u005Cb")
